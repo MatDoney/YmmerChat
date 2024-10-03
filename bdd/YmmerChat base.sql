9dc456165a83b354d0e86e9b387e4a13f15b4256
matth <matthis.doney@gmail.com>
2024-10-03 11:57:56 +0200

    Amelioration db

--- bdd/YmmerChat base.sql
+++ bdd/YmmerChat base.sql
@@ -1,39 +1,41 @@
 create database YmmerChat;
 
+
 create user "YmmerUser"@"localhost" identified by "}KYyCV\4^JO|l@i";
-GRANT insert,update,delete ON YmmerChat.* TO 'YmmerUser'@'localhost';
+grant insert,update,delete ON YmmerChat.* TO 'YmmerUser'@'localhost';
 
 use YmmerChat;
 
 create table user (
-id int not null auto_increment,
+user_id int not null auto_increment,
 username varchar(100),
 email varchar(255),
 nom varchar(100),
 prenom varchar(100),
 num varchar(10),
 password varchar(512),
-primary key (id)
+primary key (user_id)
 
-);
+)ENGINE=InnoDB;
 
 create table conversation (
-id int not null auto_increment,
+conv_id int not null auto_increment,
 name varchar(255),
 author int,
 created_AT DateTime default now(),
-primary key (id),
-CONSTRAINT fk_user foreign key (author) references user(id) on delete cascade
-);
+primary key (conv_id)
+)ENGINE=InnoDB;
 
 create table participant (
-id int not null auto_increment,
+participant_id int not null auto_increment,
 user_id int not null,
 conversation_id int not null,
-primary key(id),
-CONSTRAINT fk_user_participants foreign key (user_id) references user(id),
-CONSTRAINT fk_conversation_participants foreign key (conversation_id) references conversation(id) on delete cascade
-);
+primary key(participant_id),
+CONSTRAINT fk_user_participants foreign key (user_id) references user(user_id),
+CONSTRAINT fk_conversation_participants foreign key (conversation_id) references conversation(conv_id) on delete cascade
+)ENGINE=InnoDB;
+
+ALTER TABLE conversation ADD CONSTRAINT fk_user foreign key (author) references participant(participant_id) on delete cascade;
 
 create table message(
 id int not null auto_increment,
@@ -41,5 +43,5 @@ texte text,
 participant_id int not null,
 date DateTime default now(),
 primary key(id),
-constraint fk_message_participants foreign key (participant_id) references participant(id) on delete cascade
-);
+constraint fk_message_participants foreign key (participant_id) references participant(participant_id) on delete cascade
+)ENGINE=InnoDB;
