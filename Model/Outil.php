c49f30a5016ae7bf5e98f0ffb9fe698ff63b8d0c
matth <matthis.doney@gmail.com>
2024-10-03 13:58:43 +0200

    Ajout méthodes outils contenant pdo innit + squellette route

--- Model/Outil.php
+++ Model/Outil.php
@@ -1,7 +1,33 @@
 <?php
-
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+/*
+ * Fichier contenant toutes les methodes outil
  */
+
+function PdoInit(): PDO {
+    try {
+
+
+        include "/bdd/db_settings";
+
+        $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+        // Data Source Name
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Mode de rapport d'erreur
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Mode de récupération par défaut
+            PDO::ATTR_EMULATE_PREPARES => false, // Désactiver l'émulation de requêtes préparées
+        ];
+
+        
+            $pdo = new PDO($dsn, $user, $pass, $options); // Création de l'objet PDO
+            return $pdo;
+        
+    } catch (Exception $ex) {
+        return $ex->getMessage();
+    }
+    
+}
+
+
+
+
 
---
+++ bdd/db_settings.php
@@ -0,0 +1,7 @@
+<?php
+    $host = 'localhost'; // Adresse de l'hôte (localhost ou adresse IP)
+    $db = 'YmmerChat'; // Nom de la base de données
+    $user = 'YmmerUser'; // Nom d'utilisateur
+    $pass = '}KYyCV\4^JO|l@i'; // Mot de passe
+    $charset = 'utf8mb4'; // Jeu de caractères
+
