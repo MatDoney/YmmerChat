da50f91a3753c70f860c21f4c195978ef10f5476
matth <matthis.doney@gmail.com>
2024-10-09 22:21:56 +0200

    debut page ajouté participant

--- Model/js/Outil.js
+++ Model/js/Outil.js
@@ -1,7 +1,4 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
- */
+
 
 function scrollToBottom(div) {
     div.scrollTop = div.scrollHeight; // Fait défiler jusqu'en bas de la div
@@ -131,4 +128,8 @@ function DeleteMessageById(message_id, site_root) {
     xhr.open("DELETE", site_root + "/Model/api/message.php?message_id=" + message_id);
 
     xhr.send();
+}
+
+function GetParticipantByConvID() {
+    
 }
---
+++ View/add_participant.php
@@ -0,0 +1,14 @@
+
+ <aside>
+ <div id="AddParticipant">
+     <h2>Ajouter Participant</h2>
+     <input type="texte" id="searchbarUser">
+     <button type="button" id="searchUser">Valider</button>
+ </div>
+ 
+     <div id="ListeParticipant">
+         <ul id="ListeParticipantUL">
+             
+         </ul>
+     </div>
+ </aside>
